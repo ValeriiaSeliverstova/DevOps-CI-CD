83dacb7e6d8fe2294e0281deac1e21e91184337e
seliverstova-valerya <seliverstova.valerya@gmail.com>
2026-04-14 22:48:35 +0300

    fix: add-private-subnets-routes

--- lesson-5/modules/ecr/ecr.tf
+++ lesson-5/modules/ecr/ecr.tf
@@ -5,4 +5,30 @@ resource "aws_ecr_repository" "lesson_5" {
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
+
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "ecr_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions = [
+      "ecr:*"
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "lesson_5_policy" {
+  repository = aws_ecr_repository.lesson_5.name
+  policy     = data.aws_iam_policy_document.ecr_policy.json
+}
+
+
+
--- lesson-5/modules/vpc/routes.tf
+++ lesson-5/modules/vpc/routes.tf
@@ -21,3 +21,25 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+# Створюємо окрему маршрутну таблицю для приватних підмереж
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+# Додаємо маршрут для виходу з приватних підмереж через NAT Gateway
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+# Прив'язуємо приватну маршрутну таблицю до всіх приватних підмереж
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
